fc3a8c53455cfb0ba3c635111015963e310fac3e
saarp <saarp@liveperson.com>
2020-12-15 22:39:28 +0200

    delete users

--- service/controller.js
+++ service/controller.js
@@ -10,5 +10,23 @@ exports.userController = {
         User.findOne({ id: req.params.id })
             .then(docs => { res.json(docs) })
             .catch(err => console.log(`Error getting the data from DB: ${err}`));
+    },
+    deleteUser(req,res){
+        // const user = userExistence(res, req);
+        // if(!user){
+        //     console.log("User NOT found");
+        //     return;
+        // }
+        User.deleteOne({ id: req.params.id } ) 
+            .then(docs => { console.log(`User deleted ${req.params.id}`) , res.json(docs)  })
+            .catch(err => console.log(`Error deleting restaurant from db: ${err}`));
     }
 };
+// function userExistence(res,req){
+//     const user = User.findOne({ id: req.params.id });
+//     if (!user){
+//         res.status(404).send(`Can't finde user: ${req.params.id}`);
+//         return;
+//     } 
+//     return user;
+// }
--- service/router.js
+++ service/router.js
@@ -7,3 +7,6 @@ module.exports = { userRouter };
 
 userRouter.get('/',userController.getUsers); 
 userRouter.get('/:id',userController.getUser);
+// userRouter.post('/',userController.addUser); 
+// userRouter.put('/:id',userController.updateUser); //localhostn:8080/api/users/#id
+userRouter.delete('/:id',userController.deleteUser); //localhostn:8080/api/users/#id
